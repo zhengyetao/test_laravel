fc2636c9282bc8b3f99430acafd6f5e7d4e41f34
zhengyetao <zhengyetao@126.com>
2019-02-06 21:46:14 +0800

    查看用户列表

--- weibo/app/Http/Controllers/UsersController.php
+++ weibo/app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
         
         $this->middleware('guest', [
@@ -77,5 +77,13 @@ class UsersController extends Controller
         session()->flash('session', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user->id);
+    }
+
+
+    public function index()
+    {
+        //$users = User::all();
+        $users = User::paginate(5);
+        return view('users.index', compact('users'));
     }
 }
